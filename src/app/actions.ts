643f35b5e94be4dbfca08d1586e210f5d5b5818b
Your Name <you@example.com>
2026-07-31 20:43:20 -0300

    Fix TypeScript error

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -5,9 +5,9 @@ import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 export async function createVehicle(formData: FormData) {
-  const licensePlate = formData.get("licensePlate") as String;
-  const brand = formData.get("brand") as String;
-  const model = formData.get("model") as String;
+  const licensePlate = formData.get("licensePlate") as string;
+  const brand = formData.get("brand") as string;
+  const model = formData.get("model") as string;
   const year = parseInt(formData.get("year") as string, 10);
   const vtvExpiration = new Date(formData.get("vtvExpiration") as string);
   const insuranceExpiration = new Date(formData.get("insuranceExpiration") as string);
